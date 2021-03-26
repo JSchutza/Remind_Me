4c76398e9bad23065838d8b766c50604644e9b6c
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-03-25 19:10:26 -0500

    added the redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import ThemeChanger from './components/ThemeChanger';
 import ThemeProvider from './context/ThemeContext.js';
+
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import configureStore from './store';
+
+
+const store = configureStore();
+
+
+// for development
+if (process.env.NODE_ENV !== 'production') {
+  window.store = store;
+}
+
 
 
 // used to simplify ReactDOM.render below
@@ -12,15 +25,18 @@ function Root() {
 
   return (
     <>
+    <Provider store={store}>
       <ThemeProvider>
         <BrowserRouter>
           <ThemeChanger />
+
           {/*main content div here: */}
           <div>
             <App />
           </div>
         </ BrowserRouter>
       </ThemeProvider>
+      </Provider>
     </>
   );
 
