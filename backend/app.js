3211d301779621bbd5dcea400d6db295e402eae7
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-03-25 19:43:28 -0500

    more tweeks

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,41 @@ initApp(app);
 app.use('/api/users', users_router);
 
 
+// for production set up
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+
+    // Serve the frontend's index.html file at the root route
+    app.get('/', (request, response) => {
+        response.cookie('XSRF-TOKEN', request.csrfToken());
+        response.sendFile(path.resolve(__dirname, '../../frontend', 'build', 'index.html'));
+    });
+
+    // Serve the static assets in the frontend's build folder
+    app.use(express.static(path.resolve("../frontend/build")));
+
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
+    app.get(/^(?!\/?api).*/, (request, response) => {
+        response.cookie('XSRF-TOKEN', request.csrfToken());
+
+        response.sendFile(path.resolve(__dirname, '../../frontend', 'build', 'index.html'));
+    });
+
+}
+
+
+// development set up
+if (process.env.NODE_ENV !== 'production') {
+    app.get('/api/csrf/restore', (request, response) => {
+        response.cookie('XSRF-TOKEN', request.csrfToken());
+        response.json({});
+    });
+}
+
+
+
+
+
 
 // error handlers here:
 setErrorHandlers(app);
