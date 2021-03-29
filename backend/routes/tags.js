a6908932bb80235a3533fa7633b3fc7662c243c4
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-03-29 16:27:08 -0500

    started api to get the users notebooks

---
+++ backend/routes/tags.js
@@ -0,0 +1,22 @@
+const { express, asyncHandler, setTokenCookie, User, restoreUser, validateLogin, validateSignup } = require('../lib');
+
+
+
+
+
+// invoke router so we can use it
+const router = express.Router();
+
+
+
+
+
+// routes here:
+
+
+
+
+
+
+// exports here:
+module.exports = router;
