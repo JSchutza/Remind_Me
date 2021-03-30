09ed62ab675689c823cb089080d6f080f879ada3
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-03-29 21:25:04 -0500

    note

--- frontend/src/components/NotebookPage/NotebookPage.js
+++ frontend/src/components/NotebookPage/NotebookPage.js
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 
 
 
-
+// make a thunk to  http://localhost:5000/api/notes/notebookId
 
 const NotebookPage = () => {
   const { notebookId } = useParams();
