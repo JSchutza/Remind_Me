bcb2c2de68fec64b74ffd8f685d5150608e16823
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-03-30 13:37:16 -0500

    individual note page done?

--- frontend/src/components/NoteViewer/NoteViewer.js
+++ frontend/src/components/NoteViewer/NoteViewer.js
@@ -5,12 +5,32 @@ import { useState, useEffect } from 'react';
 
 
 const NoteViewer = ({ the_content }) => {
-
+  // state here
+  const [ title, setTitle ] = useState(the_content.title);
+  const [ content, setContent ] = useState(the_content.content);
 
   return (
     <>
-    <h1>Individual Note</h1>
-      <p>{the_content.title}</p>
+    <div>
+      <h1>{title}</h1>
+      <p>{content}</p>
+    </div>
+
+
+    <div>
+      <label>
+        Title:
+        <input
+          onChange={ (event) => setTitle(event.target.value)}
+          value={title}
+        />
+        </label>
+
+        <textarea
+          onChange={(event) => setContent(event.target.value)}
+          value={content}
+        />
+    </div>
     </>
   );
 };
