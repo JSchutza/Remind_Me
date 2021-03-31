5033070c1b087662e72f124eac0c61333b3d5629
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-03-30 20:58:18 -0500

    more styles

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,24 +1,44 @@
 import Div from '../Div';
-
+import { useState, useEffect } from 'react';
 
 
 // import styles
 import { styles } from '../Footer';
-
+import profile_photo from './profile_pic.jpg';
 
 
 
 
 function Footer() {
+// state here
+    const [ show, setShow ] = useState(false);
 
+
+    useEffect(() => {
+
+    },[show]);
+
+
+
+    const footerClickHandler = (event) => {
+        event.preventDefault();
+        setShow(true);
+    };
 
 
     return (
         <>
         <footer className={styles.main_footer}>
             <Div selectors={[styles.footer_container]}>
-            {/* have a onclick that shows a popup info component */}
-                <h1>The footer</h1>
+                <a onClick={(event) => footerClickHandler(event)}>
+                    <img src={profile_photo} />
+                </a>
+
+
+                {/* have a onclick that shows a popup info component */}
+                {
+                    show === true ? <h1>Testing</h1> : <div></div>
+                }
             </Div>
         </footer>
         </>
