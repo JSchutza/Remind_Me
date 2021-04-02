c3577882766f2e2c0fc4a9003ca84e8acdc27067
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-04-02 13:02:38 -0500

    api/notebooks/new done

--- backend/routes/notebooks.js
+++ backend/routes/notebooks.js
@@ -43,7 +43,25 @@ router.get('/specific/:notebookId(\\d+)', asyncHandler(async (request, response)
 }));
 
 
+// POST localhost:5000/api/notebooks/new
+router.post('/new', asyncHandler(async (request, response) => {
+  const { name, description, notebook_owner } = request.body;
 
+
+  const created_notebook = await Notebook.create({
+    name,
+    description,
+    notebook_owner: Number(notebook_owner)
+  });
+
+
+
+  if (!created_notebook) {
+    response.json({ error: "Could not create a notebook."});
+  }
+
+  response.json({ created_notebook });
+}));
 
 
 // exports here:
