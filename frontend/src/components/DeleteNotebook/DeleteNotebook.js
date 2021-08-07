fa4ce624f37117640d9c76866aba225566dcd456
JSchutza <joshua.schutza@pop.belmont.edu>
2021-08-06 21:07:13 -0500

    sequelize never ending loop on delete of notebook

--- frontend/src/components/DeleteNotebook/DeleteNotebook.js
+++ frontend/src/components/DeleteNotebook/DeleteNotebook.js
@@ -1,14 +1,29 @@
 
 
-import { useState } from 'react';
+// import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 
+import { thunk_deleteNotebook } from "../../thunks/notebooks.js";
 
 
+const DeleteNotebook = ({ notebookId }) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-const DeleteNotebook = () => {
+  const handleDelete = event => {
+    event.preventDefault();
+    dispatch(thunk_deleteNotebook(notebookId));
+    history.push("/profile");
+  }
+
+
   return (
     <>
+      <div>
+        <Link to={'/'} onClick={event => handleDelete(event)}> Delete </Link>
+      </div>
     </>
   )
 };
