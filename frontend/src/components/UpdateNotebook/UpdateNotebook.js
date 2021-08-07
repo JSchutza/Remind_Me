0bce706d261b85348e6b474af128eb19cffcb7c9
JSchutza <joshua.schutza@pop.belmont.edu>
2021-08-07 11:53:02 -0500

    started update notebook component

--- frontend/src/components/UpdateNotebook/UpdateNotebook.js
+++ frontend/src/components/UpdateNotebook/UpdateNotebook.js
@@ -1,16 +1,33 @@
-
-
-
 import { useState } from 'react';
 
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
+
+import { thunk_updateNotebook } from "../../thunks/notebooks.js";
 
 
 
-const UpdateNotebook = () => {
+
+
+const UpdateNotebook = ({ notebookId }) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const handleUpdate = event => {
+    event.preventDefault();
+    dispatch();
+  }
+
+
+
   return (
     <>
     </>
   )
+
 };
+
+
+
 
 export default UpdateNotebook;
