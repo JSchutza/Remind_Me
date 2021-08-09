4eb9909b6530263e6b9dfa4fee2cee65b3ca1e5d
JSchutza <joshua.schutza@pop.belmont.edu>
2021-08-09 10:39:23 -0500

    added create notebook form to modal

--- frontend/src/components/CreateNotebookForm/CreateNotebookForm.js
+++ frontend/src/components/CreateNotebookForm/CreateNotebookForm.js
@@ -4,7 +4,9 @@ import { useState } from 'react';
 
 import { useDispatch } from 'react-redux';
 
+import { thunk_createNewNotebook } from "../../thunks/notebooks.js";
 
+import { useUser } from "../../context/UserContext.js";
 
 
 
@@ -12,14 +14,18 @@ import { useDispatch } from 'react-redux';
 const CreateNotebookForm = ({ notebookId }) => {
   const [ name, setName ] = useState('');
   const [ description, setDescription ] = useState('');
-
   const dispatch = useDispatch();
+  const { isUser } = useUser();
+
+
 
   const onSubmit = event => {
     event.preventDefault();
-    const payload = { name, description, notebookId };
-    // dispatch(thunk_updateNotebook(payload));
+    const payload = { name, description, notebook_owner: isUser.id };
+    dispatch(thunk_createNewNotebook(payload));
   }
+
+
 
 
 
@@ -44,10 +50,12 @@ const CreateNotebookForm = ({ notebookId }) => {
           />
         </label>
 
-        <button type="submit" > Update </button>
+        <button type="submit" > Create </button>
       </form>
     </>
   )
 };
+
+
 
 export default CreateNotebookForm;
--- frontend/src/components/NotebooksPage/NotebooksPage.js
+++ frontend/src/components/NotebooksPage/NotebooksPage.js
@@ -9,13 +9,18 @@ import { Link } from 'react-router-dom';
 import { thunk_getNoteBooks } from "../../thunks/notebooks.js";
 import { useUser } from "../../context/UserContext.js";
 
+import CreateNotebookForm from "../CreateNotebookForm";
 import DeleteNotebook from "../DeleteNotebook";
 import UpdateNotebook from "../UpdateNotebook";
+
+
+import ReactModal from 'react-modal';
 
 
 
 
 const NotebooksPage = () => {
+  const [ showModal, setShowModal ] = useState(false);
   const { isUser } = useUser();
   const dispatch = useDispatch();
   const notebooks = useSelector(store => store.notebooksReducer.notebooks);
@@ -32,9 +37,30 @@ const NotebooksPage = () => {
   // check notebooks in the backend and send the length?
 
 
+
+  const handleCreate = event => {
+    event.preventDefault();
+    setShowModal(true);
+  }
+
+
+  const closeModal = () => {
+    setShowModal(false);
+  }
+
+
+
   return (
     <>
       <h1>Notebooks</h1>
+      <Link to={'/'} onClick={event => handleCreate(event)}> Create </Link>
+
+
+      <ReactModal isOpen={showModal} onRequestClose={closeModal} >
+        <CreateNotebookForm  />
+      </ReactModal>
+
+
       <div>
         {notebooks !== null ?
           <>
