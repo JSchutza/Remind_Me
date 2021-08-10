635978cc3340691829b3af51e1f4fa9ef04137b0
JSchutza <joshua.schutza@pop.belmont.edu>
2021-08-10 10:46:28 -0500

    took out history.push() call in delete notebook

--- frontend/src/components/DeleteNotebook/DeleteNotebook.js
+++ frontend/src/components/DeleteNotebook/DeleteNotebook.js
@@ -1,21 +1,20 @@
 
-
-// import { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
 
 import { thunk_deleteNotebook } from "../../thunks/notebooks.js";
 
 
+
+
 const DeleteNotebook = ({ notebookId }) => {
   const dispatch = useDispatch();
-  const history = useHistory();
+
 
   const handleDelete = event => {
     event.preventDefault();
     dispatch(thunk_deleteNotebook(notebookId));
-    history.push("/profile");
   }
 
 
