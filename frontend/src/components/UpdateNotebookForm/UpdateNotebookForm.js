30732573fba5e162e6e05b5398305310d253c5c2
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-09-10 12:20:06 -0700

    update to show backend errors for notebook update

--- frontend/src/components/UpdateNotebookForm/UpdateNotebookForm.js
+++ frontend/src/components/UpdateNotebookForm/UpdateNotebookForm.js
@@ -1,7 +1,7 @@
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { thunk_updateNotebook } from "../../thunks/notebooks.js";
 
@@ -14,22 +14,42 @@ import styles from './updatenotebookform.module.css';
 const UpdateNotebookForm = ({ notebookId, closeModal }) => {
   const [ name, setName ] = useState('');
   const [ description, setDescription ] = useState('');
-
+  const [ error, setError ] = useState([]);
+  const errors = useSelector(store => store.errorReducer.errors);
   const dispatch = useDispatch();
+
+
+
+  useEffect(() => {
+    if (errors !== null) {
+      setError(errors)
+    }
+  }, [errors]);
+
+
+
+
 
 
   const onSubmit = event => {
     event.preventDefault();
     const payload = { name, description, notebookId };
     dispatch(thunk_updateNotebook(payload));
-    closeModal();
+    // closeModal();
   }
+
 
 
 
 
   return (
     <>
+      <div>
+        {error.map(eachError => (
+          <li> {eachError}</li>
+        ))}
+      </div>
+
     <div className={styles.updatenotebook_wrap} >
       <form className={styles.updatenotebook_form} onSubmit={onSubmit} >
         <label>
