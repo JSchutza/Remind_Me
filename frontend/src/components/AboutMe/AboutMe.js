446d022ef244c4ef816afc46ca51d2d48da949cd
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-09-13 15:59:15 -0700

    added more to about me component

--- frontend/src/components/AboutMe/AboutMe.js
+++ frontend/src/components/AboutMe/AboutMe.js
@@ -14,7 +14,7 @@ const AboutMe = () => {
   return (
     <>
     <div className={styles.aboutme_wrap}>
-      <p> Joshua Schutza </p>
+      <h1> Joshua Schutza </h1>
 
         <a href='https://github.com/JSchutza/Remind_Me' target="_blank" >
           <li> GitHub </li>
@@ -23,6 +23,14 @@ const AboutMe = () => {
         <NavLink exact to="/readme" >
           <li>README</li>
         </NavLink>
+
+          <img
+            align="center"
+            src="https://github-readme-stats.vercel.app/api?username=JSchutza&show_icons=true&theme=gotham&include_all_commits=true&count_private=true"
+            alt="Joshua Schutza"
+          />
+
+        <p> Full Stack Software Engineer | React | Redux | JavaScript | Python | Flask | Express | PostgreSQL | Sequelize | SQLAlchemy </p>
 
     </div >
     </>
