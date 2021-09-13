bba8735703fcc8219b529cdd9a5963e61d732d5f
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-09-13 16:39:35 -0700

    more about me styles'

--- frontend/src/components/AboutMe/AboutMe.js
+++ frontend/src/components/AboutMe/AboutMe.js
@@ -16,6 +16,7 @@ const AboutMe = () => {
     <div className={styles.aboutme_wrap}>
       <h1> Joshua Schutza </h1>
 
+      <div className={styles.aboutme_links} >
         <a href='https://github.com/JSchutza/Remind_Me' target="_blank" >
           <li> GitHub </li>
         </a>
@@ -23,8 +24,10 @@ const AboutMe = () => {
         <NavLink exact to="/readme" >
           <li>README</li>
         </NavLink>
+      </div>
 
           <img
+            className={styles.aboutme_img}
             align="center"
             src="https://github-readme-stats.vercel.app/api?username=JSchutza&show_icons=true&theme=gotham&include_all_commits=true&count_private=true"
             alt="Joshua Schutza"
