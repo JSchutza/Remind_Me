9162dc5bd5b058235b350f9a43d45432eed760fb
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-12-14 17:01:42 -0800

    remove-un-used-code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,7 @@ import Footer from './components/Footer';
 // import the reset css file here
 import './reset.css';
 
-// import main css file here
-import styles from './main.module.css';
+
 
 
 // component definitions here:
