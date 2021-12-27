038de41d9070b9848a9cbd49a28f89805ec7094c
JSchutza <51064354+JSchutza@users.noreply.github.com>
2021-12-27 15:41:06 -0800

    started-dynamic-file-detection

--- frontend/src/context/EditorContext.js
+++ frontend/src/context/EditorContext.js
@@ -16,17 +16,82 @@ const EditorProvider = ({ children }) => {
 
 
 
-  const EachEditor = ({ content, handleEditorChange }) => {
+
+  const EachEditor = ({ title='scratch.js',  content, handleEditorChange }) => {
+    const map = {
+      javascript: {
+        type: 'JavaScript',
+        support: true,
+        filename: '.js',
+      },
+      python: {
+        type: 'Python',
+        support: false,
+        filename: '.py',
+      },
+      markdown: {
+        type: 'Markdown',
+        support: false,
+        filename: '.md',
+      },
+      typescript: {
+        type: 'TypeScript',
+        support: true,
+        filename: '.ts',
+      },
+      c: {
+        type: 'C++',
+        support: false,
+        filename: '.cpp',
+      },
+      html: {
+        type: 'HTML',
+        support: true,
+        filename: '.html',
+      },
+    };
+
+    const [ tabtitle, setTabTitle ] = useState(title);
+    const [ fileType, setFileType ] = useState(map[language] ? map[language].type : 'Text');
+    const [ support, setSupport ] = useState(map[language] ? map[language].support : false);
+
+
+
+
+    useEffect(() => {
+      const toArr = title.split(' ');
+      const fileExtension = map[language]?.filename;
+
+      if (!title.length || !tabtitle.length) {
+        setTabTitle('scratch' + fileExtension);
+        return;
+      }
+
+      setTabTitle(toArr.join('_') + fileExtension);
+    },[title]);
+
+
+
     return (
-      <Editor
-        height='90vh'
-        defaultLanguage={language}
-        value={content}
-        onChange={handleEditorChange}
-        theme={theme}
-      />
+      <>
+        <h3> {fileType} </h3>
+        <p> {tabtitle} </p>
+        <p> {support ? 'IntelliSense support: on'  : 'IntelliSense support: off'} </p>
+
+
+        <Editor
+          height='90vh'
+          defaultLanguage={language}
+          value={content}
+          onChange={handleEditorChange}
+          theme={theme}
+        />
+      </>
     );
   };
+
+
+
 
 
 
@@ -46,7 +111,11 @@ const EditorProvider = ({ children }) => {
     </EditorContext.Provider>
   );
 
+
+
 };
+
+
 
 // exports here:
 export { useEditor, EditorProvider };
